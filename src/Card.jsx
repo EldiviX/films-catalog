de48b9e411bc9cc29afa25853288222ec4d9fde9
EldiviX <danya.asanov1@gmail.com>
2023-12-29 00:42:46 +0300

    Детали

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,24 +1,33 @@
 import * as React from 'react';
 import GradeOutlinedIcon from '@mui/icons-material/GradeOutlined';
 import IconButton from '@mui/material/IconButton';
+import { Link } from 'react-router-dom';
 
 export default function Card({film}) {
-    const url = `https://image.tmdb.org/t/p/w500${film.poster_path}`
+    const url = `https://image.tmdb.org/t/p/w500${film.backdrop_path}`;
+    const styleLink = { textDecoration: 'none', color: 'black'};
     return (
-        <div className="card">
-            <img className='card_pic' src={url} alt="" />
-            <div className="card-description">
-                <div className="card_main">
-                    <div className="card_main-name">{film.title}</div>
-                    <div className="card_main-rating">Рейтинг {film.vote_average.toFixed(1)}</div>
+        <div style={{ textDecoration: 'none', color: 'black', maxHeight: '280px', maxWidth: '320px', marginRight: '10px', marginBottom: '10px' }}>
+            <div className="card">
+                <Link to={`/details/${film.id}`} style={styleLink}>
+                    <img className='card_pic' src={url} alt="" />
+                </Link>
+                <div className="card-description">
+                    <Link to={`/details/${film.id}`} style={styleLink}>
+                        <div className="card_main">
+                            <div className="card_main-name">{film.title}</div>
+                            <div className="card_main-rating">Рейтинг {film.vote_average.toFixed(1)}</div>
+                        </div>
+                    </Link>
+                    <IconButton
+                        onClick={(event) => event.stopPropagation()}
+                        className='card_main-like'
+                        size='small'
+                        style={{position:"absolute", right:"15px", bottom:"5px", width:"40px", height:"40px", marginTop:"-8px", marginRight:"-10px"}}
+                    >
+                        <GradeOutlinedIcon />
+                    </IconButton>
                 </div>
-                <IconButton
-                    className='card_main-like'
-                    size='small'
-                    style={{width:"40px", height:"40px", marginTop:"-8px", marginRight:"-10px"}}
-                >
-                    <GradeOutlinedIcon />
-                </IconButton>
             </div>
         </div>
     )
--- src/Details.jsx
+++ src/Details.jsx
@@ -1,7 +1,12 @@
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import LocalMoviesSharpIcon from '@mui/icons-material/LocalMoviesSharp'
+import { Link } from "react-router-dom";
+import './Details.css';
 
 export default function Details() {
+    const { id } = useParams();
     const [details, setDetails] = useState([]);
     const [credits, setCredits] = useState([]);
     // const { id } = useParams();
@@ -14,16 +19,98 @@ export default function Details() {
             }
         };
           
-        fetch('https://api.themoviedb.org/3/movie/movie_id?language=ru', options)
+        fetch(`https://api.themoviedb.org/3/movie/${id}?language=ru`, options)
             .then(response => response.json())
-            .then(response => console.log(response))
+            .then(response => {
+                setDetails(response);
+                console.log(response);
+            })
+            .catch(err => console.error(err));
+
+        fetch(`https://api.themoviedb.org/3/movie/${id}/credits?language=ru`, options)
+            .then(response => response.json())
+            .then(response => {
+                setCredits(response);
+                console.log(response);
+            })
             .catch(err => console.error(err));
     }, [])
     console.log(details);
     console.log(credits);
+    const url = `https://image.tmdb.org/t/p/w500${details.poster_path}`
+    const voteAverage = details.vote_average !== undefined ? details.vote_average.toFixed(1) : null;
+
+    function convertMinutesToTime(totalMinutes) {
+        const date = new Date(0);
+        date.setMinutes(totalMinutes);
+      
+        const hours = date.getUTCHours();
+        const minutes = date.getUTCMinutes();
+      
+        const formattedHours = hours < 10 ? `0${hours}` : hours;
+        const minutesWithLeadingZero = minutes < 10 ? `0${minutes}` : minutes;
+      
+        return `${formattedHours}:${minutesWithLeadingZero}`;
+      }
+
     return (
-        <div>
+        <>
+            <header>
+                <div className="header__logo">
+                    <Link to={'/'}>
+                        <button>{/* onClick={} */}
+                            <LocalMoviesSharpIcon fontSize='medium' style={{marginBottom: '-5px', marginRight: '5px'}}/>
+                            Фильмы
+                        </button>
+                    </Link>
+                </div>
+                <div className="header__exit">
+                    <button>{/* onClick={} */}
+                        <AccountCircleIcon fontSize='large' style={{marginBottom: '-5px'}}/>
+                    </button>
+                </div>
+            </header>
             
-        </div>
+            <div className="main" style={{marginLeft: '70px'}}>
+                <div className="details-img">
+                    <img src={url} alt="" style={{width: '350px'}}/>
+                </div>
+                <div className="details">
+                    <div className="details-head">
+                        <div className="details-name">{details.title}</div>
+                        <div className="details-rating">Рейтинг {voteAverage}</div>
+                    </div>
+                    <div className="details-info-name">О фильме</div>
+                    <div className="details-text">{details.overview}</div>
+                    <div className="details-info">
+                        <div className="details-info_title">Год выпуска: </div>
+                        <div className="details-info_ops">{details.release_date && details.release_date.substring(0, 4)}</div>
+                    </div>
+                    <div className="details-info">
+                        <div className="details-info_title">Бюджет: </div>
+                        <div className="details-info_ops">{details.budget && details.budget}$</div>
+                    </div>
+                    <div className="details-info">
+                        <div className="details-info_title">Доход: </div>
+                        <div className="details-info_ops">{details.revenue && details.revenue}$</div>
+                    </div>
+                    <div className="details-info">
+                        <div className="details-info_title">Жанр: </div>
+                        <div className="details-info_ops">
+                            {details.genres && details.genres.map((genre, index) => (
+                                <span key={index}>
+                                    {genre.name}
+                                    {index < details.genres.length - 1 && ', '}
+                                </span>
+                            ))}
+                        </div>
+                    </div>
+                    <div className="details-info">
+                        <div className="details-info_title">Время: </div>
+                        <div className="details-info_ops">{details.runtime} мин. / {convertMinutesToTime(details.runtime)}</div>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 }
